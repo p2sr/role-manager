ea9bda972cc51aab666c61c6ce5a6e40724e2188
Walker Knapp <walker@walkerknapp.me>
2026-02-14 16:57:33 -0500

    allow configuration from environment variables

--- src/config.rs
+++ src/config.rs
@@ -10,7 +10,19 @@ pub struct Config {
 }
 
 pub fn load_config() -> Config {
-    let config_content = fs::read_to_string("config.toml").expect("No config.toml found.");
-
-    toml::from_str(config_content.as_str()).expect("Failed to deserialize config.toml")
+    if let Ok(config_contents) = fs::read_to_string("config.toml") {
+        toml::from_str(config_contents.as_str()).expect("Failed to deserialize config.toml")
+    } else {
+        if let Ok(discord_application_id) = std::env::var("DISCORD_APPLICATION_ID")
+            && let Ok(discord_bot_token) = std::env::var("DISCORD_BOT_TOKEN")
+            && let Ok(database_url) = std::env::var("DATABASE_URL") {
+            Config {
+                discord_application_id: discord_application_id.parse().unwrap(),
+                discord_bot_token,
+                database_url
+            }
+        } else {
+            panic!("Neither config.toml or environment variables (DISCORD_APPLICATION_ID, DISCORD_BOT_TOKEN, and DATABASE_URL) could be found.");
+        }
+    }
 }
